b085fcadd8489fbf082fb5deb12ad6cc941bad67
atakan him <atakanhim@gmail.com>
2023-03-23 03:46:09 +0300

    checkout checkin

---
+++ proje-profit/Controllers/HistoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace proje_profit.Controllers
+{
+    public class HistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CheckOutView(int id)
+        {
+            return View();
+        }
+        public IActionResult CheckInView(int id)
+        {
+            return View();
+        }
+    }
+}
